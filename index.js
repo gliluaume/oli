838756011231430e093e4cc3fd500f9e15498fd7
gliluaume <guillaume.salicis@gmail.com>
2024-02-12 21:46:55 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const { XMLParser, XMLBuilder, XMLValidator } = require("fast-xml-parser");
-var http = require('http');
-var fs = require('fs');
-const { deburr, kebabCase } = require('lodash')
-
+const { XMLParser } = require("fast-xml-parser");
+const http = require("http");
+const fs = require("fs");
+const { deburr, kebabCase } = require("lodash");
 
 const getMp3 = (url, name) => {
-    var file = fs.createWriteStream(name);
-    var request = http.get(url, function (response) {
-        response.pipe(file);
-        // console.log("response.statusCode", response.statusCode)
-        /* if (response.statusCode < 400) {
-            response.pipe(file);
-        } else {
-            console.warn("error for url", url)
-        } */
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(name);
+    http.get(url, (response) => {
+      response.pipe(file);
+      response.on("end", (payload) => {
+        resolve(payload);
+      });
+      response.on("error", (err) => {
+        console.error(err);
+        reject(err);
+      });
     });
-}
+  });
+};
 
-// getMp3("http://media.radiofrance-podcast.net/podcast09/19721-04.09.2018-ITEMA_21796903-2018F33264S0248-21.mp3", "test.mp3")
-
-const RSS = "https://radiofrance-podcast.net/podcast09/rss_19721.xml"
 const getRss = async (url) => {
-    const response = await fetch(RSS);
-    if (response.ok) {
-        const xmlData = await response.text();
+  const response = await fetch(url);
+  if (response.ok) {
+    const xmlData = await response.text();
 
-        const parser = new XMLParser();
-        let data = parser.parse(xmlData);
-        return data.rss.channel.item.map(obj => {
-            return {
-                title: obj.title,
-                filename: kebabCase(deburr(obj.title)) + ".mp3",
-                url: obj.guid.startsWith("http") ? obj.guid : `http://media.radiofrance-podcast.net/podcast09/${obj.guid}.mp3`,
-                customUrl: !obj.guid.startsWith("http"),
-            }
-        });
-    }
-}
+    const parser = new XMLParser();
+    const data = parser.parse(xmlData);
+    return data.rss.channel.item.map((obj) => {
+      return {
+        title: obj.title,
+        filename: kebabCase(deburr(obj.title)) + ".mp3",
+        url: obj.guid.startsWith("http")
+          ? obj.guid
+          : `http://media.radiofrance-podcast.net/podcast09/${obj.guid}.mp3`,
+        customUrl: !obj.guid.startsWith("http"),
+      };
+    });
+  }
+};
 
 (async () => {
-    const data = await getRss();
-    data.filter(ff => !ff.customUrl).slice(0,30).forEach(entry => {
-        console.log(`Getting ${entry.title} at ${entry.url}`);
-        getMp3(entry.url, entry.filename);
-    })
-})()
-
+  // getMp3("http://media.radiofrance-podcast.net/podcast09/19721-04.09.2018-ITEMA_21796903-2018F33264S0248-21.mp3", "test.mp3")
+  const RSS = "https://radiofrance-podcast.net/podcast09/rss_19721.xml";
+  const data = await getRss(RSS);
+  const promises = data.map((entry) => getMp3(entry.url, entry.filename));
+  await Promise.allSettled(promises);
+})();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Oli RSS read
+
+Lit le flux RSS du podcast Oli puis récupère les fichiers mp3 listés.
